b43881fe9633c276abf66e820942424ea2b2b8cc
public518 <public518@public518.com>
2021-05-09 18:42:58 +0000

    clear

--- clear.sh
+++ clear.sh
@@ -1,1 +1,20 @@
-cp setup/
+#!/bin/bash
+SRC_FOLDER=src
+
+# clear CMakeLists.txt from sub sorce folder
+for dir in $(ls src)
+do
+   echo 'delete CMakeLists.txt from '$SRC_FOLDER'/'$dir
+   rm $SRC_FOLDER/$dir/CMakeLists.txt
+done
+
+# clear the include and lib folder
+echo remove build/
+rm build -rf
+echo remove includ/
+rm include -rf
+echo remove lib/
+rm lib -rf
+echo remove bin/
+rm bin -rf
+
