7ff8d4ca976910e891a6ec0839f5f25f64af2ca0
public518 <public518@public518.com>
2021-05-09 12:42:18 +0000

    use build.sh to build autoly

--- mimiLib/build.sh
+++ mimiLib/build.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-for dir in $(ls src)
+SRC_FOLDER=src
+SUB_CMAKE_DIR=config/subCmake/CMakeLists.txt
+
+# copy sub CMakeLists.txt to sub sorce folder
+for dir in $(ls $SRC_FOLDER)
 do
-        echo 'copy CMakeLists.txt to src/'$dir
-        cp config/subCmake/CMakeLists.txt src/$dir/CMakeLists.txt
+        echo 'copy '$SUB_CMAKE_DIR' to '$SRC_FOLDER'/'$dir
+        cp $SUB_CMAKE_DIR $SRC_FOLDER/$dir/CMakeLists.txt
 done
 
+# make dir build to build project
 if [ ! -d build ];then
 	mkdir build
 else
    echo dir build exist
 fi
 
+# clear the include folder
 rm include -rf
 
+# build project and remove build folder
 cd build && cmake .. && make && cd .. && rm build -rf 
 
+# remove CMakeLists.txt from sub sorce folder
 for dir in $(ls src)
 do
-   echo 'delete CMakeLists.txt from src/'$dir
-   rm  src/$dir/CMakeLists.txt
+   echo 'delete CMakeLists.txt from '$SRC_FOLDER'/'$dir
+   rm  $SRC_FOLDER/$dir/CMakeLists.txt
 done
