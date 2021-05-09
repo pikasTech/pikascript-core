accac4e41bd8aa2bc225b825757ceca05771ddf8
public518 <public518@public518.com>
2021-05-09 12:50:13 +0000

    change work dir

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,7 @@ docker build -t $DOCKER_NAME .
 
 docker run -itd \
 	--name $DOCKER_NAME \
-	-v $PWD/../:/root/dev  \
+	-v $PWD/../:/root/dev/$DOCKER_NAME\
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY \
 	--restart=always \
