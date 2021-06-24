d7601d9d1f6357544004e4a6160fe0041f69ec9c
lyon <645275592@qq.com>
2021-06-24 11:04:39 +0800

    print is tested ok

--- mimiSH/Src/shApp_print.c
+++ mimiSH/Src/shApp_print.c
@@ -7,12 +7,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ROOT_PTR argc - 1
+#define PROCESS_RANGE_START 1
+#define PROCESS_RANGE_END argc - 3
+#define PRINT_NAME argc - 2
+
 void *app_print(int argc, char **argv)
 {
-	DMEM *memOut = DynMemGet(sizeof(char) * 256);
-    mimiProcess_t *root = (mimiProcess_t *)atoi(argv[argc - 1]);
-    for (int i = 1; i <= argc - 2; i++)
+    DMEM *memOut = DynMemGet(sizeof(char) * 256);
+    mimiProcess_t *root = (mimiProcess_t *)atoi(argv[ROOT_PTR]);
+    mimiProcess_t *processNow = root;
+    for (int i = PROCESS_RANGE_START; i <= PROCESS_RANGE_END; i++)
     {
+        processNow = processNow->getPtr(processNow, argv[i]);
     }
-	return (void *)memOut;
+    char *printStr = processNow->argPrint(processNow, argv[PRINT_NAME]);
+    memcpy(memOut->addr, printStr, ARG_CONTANT_LENGTH);
+    strPrint(memOut->addr, "\r\n");
+    return (void *)memOut;
 }
