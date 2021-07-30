b95997a43c5d9f55d55236d459277c5608bf4f66
lyon tab s7 <645275593@qq.com>
2021-07-30 13:30:24 +0800

    add test

--- mimiTest/test_MimiObj.c
+++ mimiTest/test_MimiObj.c
@@ -48,6 +48,22 @@ void add(MimiObj *obj, Args *args)
     method_returnInt(args, val1 + val2);
 }
 
+static void init_test(MimiObj *self, Args *args)
+{
+    obj_defineMethod(self, "hello(name:string, isShow:int)", hello);
+    obj_defineMethod(self, "hello2(name1:string, name2:string, name3:string, isShow:int)", hello2);
+    obj_defineMethod(self, "testFloat(val1:float, val2:float, isShow:int):float", testFloat);
+    obj_defineMethod(self, "add(val1:int, val2:int):int", add);
+}
+
+MimiObj *New_MimiObj_test(Args *args)
+{
+    MimiObj *self = New_MimiObj_sys(args);
+    obj_setPtr(self, "classPtr", New_MimiObj_test);
+    init_test(self, args);
+    return self;
+}
+
 int TEST_MimiObj(int isShow)
 {
     {
@@ -75,91 +91,80 @@ int TEST_MimiObj(int isShow)
         }
         obj_deinit(process);
     }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_defineMethod(obj, "hello(name:string, isShow:int)", hello);
-    //     obj_setInt(obj, "isShow", isShow);
-    //     obj_run(obj, "hello(name = 'world', isShow = isShow)");
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_defineMethod(obj, "hello2(name1:string, name2:string, name3:string, isShow:int)", hello2);
-    //     obj_setInt(obj, "isShow", isShow);
-    //     obj_run(obj, "hello2(name2 = 'tom', \
-    //                           name1 = 'john', \
-    //                           name3 = 'cat', \
-    //                           isShow = isShow) ");
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_importAndSetObj(obj, "hello", New_MimiObj);
-    //     obj_defineMethod(obj, "hello.hello2(name1:string, name2:string, name3:string, isShow:int)", hello2);
-    //     obj_setInt(obj, "isShow", isShow);
-    //     obj_run(obj, "hello.hello2(name2 = 'tom', \
-    //                                 name1 = 'john', \
-    //                                 name3 = 'cat', \
-    //                                 isShow = isShow) ");
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_importAndSetObj(obj, "hello", New_MimiObj);
-    //     obj_setStr(obj, "name1", "john");
-    //     obj_setInt(obj, "isShow", isShow);
-    //     obj_defineMethod(obj, "hello.hello2(name1:string, name2:string, name3:string, isShow:int)",
-    //                      hello2);
-    //     obj_run(obj, "hello.hello2(name2 = 'tom', name1 = name1, name3 = 'cat', isShow = isShow) ");
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_defineMethod(obj, "testFloat(val1:float, val2:float, isShow:int):float", testFloat);
-    //     obj_setInt(obj, "isShow", isShow);
-    //     obj_setFloat(obj, "val2", 3.11);
-    //     obj_run(obj, "res = testFloat(val1 = 3.22,val2 = val2,isShow = isShow)");
-    //     float res = obj_getFloat(obj, "res");
-    //     if (isShow)
-    //     {
-    //         printf("the res is %f \r\n", res);
-    //     }
-    //     if (6.33 - res > 0.001)
-    //     {
-    //         return 2;
-    //     }
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_defineMethod(obj, "add(val1:int, val2:int):int", add);
-    //     obj_run(obj, "res = add(val1 = 1, val2 = 2)");
-    //     int res = obj_getInt(obj, "res");
-    //     if (isShow)
-    //     {
-    //         printf("the res is %d.\r\n", res);
-    //     }
-    //     if (3 != res)
-    //     {
-    //         return 1;
-    //     }
-    //     obj_deinit(obj);
-    // }
-    // {
-    //     MimiObj *obj = New_MimiObj_sys(NULL);
-    //     obj_defineMethod(obj, "add(val1:int, val2:int):int", add);
-    //     obj_run(obj, "res = add(1, 2)");
-    //     int res = obj_getInt(obj, "res");
-    //     if (isShow)
-    //     {
-    //         printf("the res is %d.\r\n", res);
-    //     }
-    //     if (3 != res)
-    //     {
-    //         return 3;
-    //     }
-    //     obj_deinit(obj);
-    // }
+    {
+        MimiObj *obj = New_MimiObj_test(NULL);
+        obj_setInt(obj, "isShow", isShow);
+        obj_run(obj, "hello(name = 'world', isShow = isShow)");
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_test(NULL);
+        obj_setInt(obj, "isShow", isShow);
+        obj_run(obj, "hello2(name2='tom', name1='john', name3='cat', isShow=isShow) ");
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_sys(NULL);
+        obj_importAndSetObj(obj, "hello", New_MimiObj_test);
+        obj_setInt(obj, "isShow", isShow);
+        obj_run(obj, "hello.hello2(name2 = 'tom', \
+                                    name1 = 'john', \
+                                    name3 = 'cat', \
+                                    isShow = isShow) ");
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_sys(NULL);
+        obj_importAndSetObj(obj, "hello", New_MimiObj_test);
+        obj_setStr(obj, "name1", "john");
+        obj_setInt(obj, "isShow", isShow);
+        obj_run(obj, "hello.hello2(name2 = 'tom', name1 = name1, name3 = 'cat', isShow = isShow) ");
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_test(NULL);
+        obj_setInt(obj, "isShow", isShow);
+        obj_setFloat(obj, "val2", 3.11);
+        obj_run(obj, "res = testFloat(val1 = 3.22,val2 = val2,isShow = isShow)");
+        float res = obj_getFloat(obj, "res");
+        if (isShow)
+        {
+            printf("the res is %f \r\n", res);
+        }
+        if (6.33 - res > 0.001)
+        {
+            return 2;
+        }
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_test(NULL);
+        obj_run(obj, "res = add(val1 = 1, val2 = 2)");
+        int res = obj_getInt(obj, "res");
+        if (isShow)
+        {
+            printf("the res is %d.\r\n", res);
+        }
+        if (3 != res)
+        {
+            return 1;
+        }
+        obj_deinit(obj);
+    }
+    {
+        MimiObj *obj = New_MimiObj_test(NULL);
+        obj_run(obj, "res = add(1, 2)");
+        int res = obj_getInt(obj, "res");
+        if (isShow)
+        {
+            printf("the res is %d.\r\n", res);
+        }
+        if (3 != res)
+        {
+            return 3;
+        }
+        obj_deinit(obj);
+    }
     {
         MimiObj *sys = New_MimiObj_sys(NULL);
         int a = 0;
