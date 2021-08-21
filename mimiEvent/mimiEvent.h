a4346a836729bcb038d936985a0e4e343cd90263
lyon tab s7 <645275593@qq.com>
2021-08-21 15:33:48 +0800

    remove mimidevice and mimievent

--- mimiEvent/mimiEvent.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MimiObj_Event__H
-#define _MimiObj_Event__H
-#include "dataMemory.h"
-#include "MimiObj.h"
-
-MimiObj *New_MimiObj_Event(Args *args);
-
-#endif
--- mimiEvent/mimiFansInfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MimiObj_FansInfo__H
-#define _MimiObj_FansInfo__H
-#include "dataMemory.h"
-#include "MimiObj.h"
-
-MimiObj *New_MimiObj_FansInfo(Args *args);
-#endif
