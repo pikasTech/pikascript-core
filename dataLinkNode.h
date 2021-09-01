30ff07c8106e6ee90363a9a2c0bae15fbc9cbd67
lyon <lyon@catgit.com>
2021-09-01 08:25:30 +0800

    removen no used link fn

--- dataLinkNode.h
+++ dataLinkNode.h
@@ -12,8 +12,6 @@ struct Class_linkNode
 
 void linkNode_deinit(LinkNode *self);
 void linkNode_init(LinkNode *self, void *args);
-int64_t linkNode_getId(LinkNode *self);
-int32_t linkNode_isId(LinkNode *self, int64_t id);
 
 LinkNode *New_linkNode(void *args);
 #endif
